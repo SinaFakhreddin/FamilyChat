28b2102405c2dde3d2496e31e421bdd27cdc1e1b
Sina Fakhreddin <sina.fakhreddin@gmail.com>
2022-06-03 16:30:27 +0430

    Add Ul to CartBody

--- src/Components/CartBody.js
+++ src/Components/CartBody.js
@@ -1,50 +1,46 @@
-import React, { Component, PureComponent } from 'react';
-import { connect } from 'react-redux';
-
+import React, { Component, PureComponent } from "react";
+import { connect } from "react-redux";
 
 class CartBody extends Component {
-
-
-  
-
   render() {
-      let {
-        productImgSelected,
-        productNameSelected,
-        productcostSelected,
-        productquantitySelected,
-        productBrand,
-        productBranch,
-        MyCartInfo
-      }=this.props
+    let {
+      productImgSelected,
+      productNameSelected,
+      productcostSelected,
+      productquantitySelected,
+      productBrand,
+      productBranch,
+      MyCartInfo,
+    } = this.props;
 
     return (
-        
-          <div className="cart-body">
-          <div className="cart-info-img">
-            <img
-              src={productImgSelected}
-              style={{ height:'40px' }}
-              alt=""
-            />
-          </div>
-          <div className='info-product-selected'>
-            <span>{productNameSelected||productBrand||productBranch}</span>
-            <em>${productcostSelected}</em>
-            <em>{productquantitySelected}</em>
-            <em>${`${productcostSelected*productquantitySelected}`}</em>
+      <div className="cart-body">
+        <div className="cart-info-img">
+          <img src={productImgSelected} style={{ height: "40px" }} alt="" />
+          <div>
+            <ul>
+              <li>test</li>
+              <li>test2</li>
+              <li>test3</li>
+              <li>test4</li>
+            </ul>
           </div>
         </div>
-        
+        <div className="info-product-selected">
+          <span>{productNameSelected || productBrand || productBranch}</span>
+          <em>${productcostSelected}</em>
+          <em>{productquantitySelected}</em>
+          <em>${`${productcostSelected * productquantitySelected}`}</em>
+        </div>
+      </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    MyCartInfo: state.cartState,
+  };
+};
 
-const mapStateToProps =(state)=>{
-  return{
-    MyCartInfo:state.cartState
-  }
-}
-
-export default connect(mapStateToProps)(CartBody)
+export default connect(mapStateToProps)(CartBody);
